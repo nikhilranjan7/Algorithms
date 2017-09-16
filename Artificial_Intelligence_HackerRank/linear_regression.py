0cec07b77a35911d880028c61708db49935dc6b3
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-09-17 01:33:02 +0530

    Test recursive

--- Artificial_Intelligence_HackerRank/linear_regression.py
+++ Artificial_Intelligence_HackerRank/linear_regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 CSV_COLUMNS = ["feature","label"]
 a = pd.read_csv("data1.csv",delimiter=",",header=None)
@@ -9,8 +10,10 @@ a.columns = CSV_COLUMNS
 
 x = np.array(a.drop(['label'],1))
 y = np.array(a['label'])
-x = preprocessing.scale(x)
 
+plt.scatter(x,y)
+plt.show()
+x = preprocessing.scale(x)
 regr = LinearRegression()
 
 regr.fit(x, y)
