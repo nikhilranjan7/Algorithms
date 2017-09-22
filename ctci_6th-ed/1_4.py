4a1cad3829b10807c10474f91fe65032e069bbee
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-09-22 23:35:53 +0530

    solve palindrome permutation

---
+++ ctci_6th-ed/1_4.py
@@ -0,0 +1,24 @@
+def palindrome_permutation(string):
+    string = string.lower()
+    string = string.split()
+    a = {}
+    string = "".join(string)
+    for i in string:
+        a[i] = 0
+    for j in string:
+        a[j] += 1
+    odd = 0
+    if len(string)%2 == 0:
+        for k in a:
+            if a[k]%2 !=0:
+                return False
+    else:
+        for k in a:
+            if a[k]%2!=0:
+                odd +=1
+            if odd > 2:
+                return False
+
+    return True
+
+print(palindrome_permutation("Tacto Coa"))
